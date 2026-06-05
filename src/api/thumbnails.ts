08ef24d96b841cb66997846f302b5c3d0248255e
bwhenry <bwhenry19@gmail.com>
2026-06-04 20:04:52 -0600

    add upload thumbnails

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -13,11 +13,12 @@ type Thumbnail = {
 const videoThumbnails: Map<string, Thumbnail> = new Map();
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
+  let { videoId } = req.params as { videoId?: string };
+  videoId = videoId?.slice(1);
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
   }
-
+  console.log(videoId);
   const video = getVideo(cfg.db, videoId);
   if (!video) {
     throw new NotFoundError("Couldn't find video");
@@ -47,7 +48,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const data = await req.formData();
+  const image = data.get("thumbnail");
+  if (!(image instanceof File))
+  {
+    throw new BadRequestError("No thumbnail file");
+  }
+  const MAX_UPLOAD_SIZE = 10 << 20; //10MB
+  if (image.size > MAX_UPLOAD_SIZE) 
+  {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`,
+    );
+  }
 
-  return respondWithJSON(200, null);
+  const mediaType = image.type;
+  const buffer = await image.arrayBuffer();
+
+  const videoData = getVideo(cfg.db, videoId);
+  if(!videoData)
+  {
+    throw new Error("Cannot find video");
+  }
+
+  if(videoData.userID != userID)
+  {
+    throw new UserForbiddenError("Not authorized to modify video");
+  }
+
+  videoThumbnails.set(videoId, {data: buffer, mediaType});
+  const url = `http://localhost:${cfg.port}/api/thumbnails/:${videoId}`
+  videoData.thumbnailURL = url;
+  updateVideo(cfg.db, videoData);
+
+  return respondWithJSON(200, videoData);
 }
